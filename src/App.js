5782d62d1a59b7436f2b1bf2ef98af74c6c0f1ca
gvbarker <gvbarker23@wabash.edu>
2023-07-01 20:24:07 -0400

    feat: assembler
    established component setup for taking in machine commands and displaying them in a 16x16 table of 'Cell' components.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import "./App.css";
 import Assembler from "./components/Assembler";
 
 function App() {
-  function data() {
-    
+  function generateAssemblerDataTable() {
+    let dataTable = Array(17).fill("00").map(row => Array(17).fill("00"));
+    for (let i=0; i<16; i++) {
+      dataTable[i+1][0]="0"+i.toString(16).toUpperCase();
+      dataTable[0][i+1]="0"+i.toString(16).toUpperCase();
+    }
+    dataTable[0][0]="";
+    let table = dataTable.slice(1, dataTable.length).map(i => i.slice(1, i.length));
+    return (Array(256).fill("00"));
+    //return (table);
   }
   return (
-    <Assembler></Assembler>
+    <>
+      <Assembler data={generateAssemblerDataTable()}></Assembler>
+    </>
   );
 }
 
