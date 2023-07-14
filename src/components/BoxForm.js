414b7270af25e06bd5d375856505bf0b59aea962
gvbarker <gvbarker23@wabash.edu>
2023-07-14 06:17:56 -0400

    feat: add code input

--- src/components/BoxForm.js
+++ src/components/BoxForm.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import Memory from "./Memory";
-import InputBox from "./InputBox";
 import cpu from "../emulator/cpu";
 import assembler from "../emulator/assembler";
 import testASM from "../emulator/testasm";
+import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
 
 export default function BoxForm() {
@@ -19,8 +19,13 @@ export default function BoxForm() {
     }
     setMemory(nextCells);
   }
+  function handleTextChange(event) {
+    setCode(event.target.value);
+  } 
   function onAssemble() {
-    emulatorASM.setCodeToAssemble(testASM.cpucode);
+    const assembly = code;
+    if (!assembly?.trim()) { return; }
+    emulatorASM.setCodeToAssemble(assembly);
     emulatorASM.assemble();
     mutateMem("asm");
   }
@@ -33,12 +38,21 @@ export default function BoxForm() {
     emulatorCPU.step();
     mutateMem("cpu");
   }
+  const test = (e) => {
+    setCode(e.target.value);
+  }
   return (
     <div className="flex bg-slate-600">
-      <InputBox/>
+      <TextField
+        multiline
+        placeholder = "Assembly..."
+        rows = { 20 }
+        value={ code }
+        onChange = { handleTextChange }
+        className="flex bg-white w-1/3 rounded-lg p-3 overflow-auto"
+      />
       <Memory 
-        data={memory} 
-        onAssemble={(() => onAssemble())}
+        data = { memory } 
       />
       <div
         className="block bg-stone-700 rounded-lg w-1/6 float-right p-2"
@@ -51,7 +65,7 @@ export default function BoxForm() {
         </div>
         <Button
           variant = "outlined" 
-          onClick={(() => onAssemble())}
+          onClick = {(() => onAssemble())}
           className="text-white block flex-none h-16 clear-both align-middle p-2"
         >
           Assemble
