c17b2d09c228488c622630c7f42019ecc9c96962
gvbarker <gvbarker23@wabash.edu>
2023-07-15 03:27:06 -0400

    feat: add header and emulator button component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="bg-red-600 border-b px-5 w-full items-center flex justify-between">
+      <span className="font-bold text-white">Brookshear Machine</span>
+    </header>
+  )
+}
+
