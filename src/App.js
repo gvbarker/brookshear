8be3dedd0ed591dfa062bfd095916670e46d7943
gvbarker <gvbarker23@wabash.edu>
2023-07-15 04:30:48 -0400

    feat: add header to main app

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import BoxForm from "./components/BoxForm";
 import assembler from "./emulator/assembler";
 import cpu from "./emulator/cpu";
+import Header from "./components/Header";
 function App() {
   const emuASM = new assembler();
   const emuCPU = new cpu();
   return (
     <>
+      <Header/>
       <BoxForm 
         asm = { emuASM }
         cpu = { emuCPU }
